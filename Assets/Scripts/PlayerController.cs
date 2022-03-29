217d3ba6cf05e3da0a1e7115a35861c9fa2170e1
Jake <jakegibson0228@gmail.com>
2022-03-29 14:40:20 -0400

    Started on PlayerController

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    
+    private Vector3 startPosition = Vector3.zero;
+    private Vector3 endPosition = Vector3.zero;
+    
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))    // swipe begins
+        {
+            var mousePos = Input.mousePosition;
+            mousePos.z = 10;
+            Debug.Log("Mouse down");
+            startPosition = Camera.main.ScreenToWorldPoint(mousePos);
+            Debug.Log("Start Position: " + startPosition);
+        }
+        if (Input.GetMouseButtonUp(0))    // swipe ends
+        {
+            var mousePos = Input.mousePosition;
+            mousePos.z = 10;
+            Debug.Log("Mouse Up");
+            endPosition = Camera.main.ScreenToWorldPoint(mousePos);
+            Debug.Log("End Position: " + endPosition);
+        }
+ 
+        if (startPosition != endPosition && startPosition != Vector3.zero && endPosition != Vector3.zero)
+        {
+            float deltaX = endPosition.x - startPosition.x;
+            float deltaY = endPosition.y - startPosition.y;
+            if ((deltaX > 5.0f || deltaX < -5.0f) && (deltaY >= -1.0f || deltaY <= 1.0f))
+            {
+                if (startPosition.x < endPosition.x) // swipe LTR
+                {
+                    Debug.Log("LTR");
+                } else // swipe RTL
+                {
+                    Debug.Log("RTL");
+                }
+            }
+            startPosition = endPosition = Vector3.zero;
+        }
+    }
+}

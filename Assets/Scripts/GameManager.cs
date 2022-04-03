03dae30dc2640cec599eb3e59939694dab692fbd
sBrunes <bruness@att.net>
2022-04-02 20:50:05 -0400

    Started On game manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> PrefabsToSelect;
+    public List<GameObject> GeneratedTerrain;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GenerateNextSection()
+    {
+
+    }
+}

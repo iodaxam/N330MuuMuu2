76201d09e8a2e7b6472887446c57f5dd5491fc46
sBrunes <bruness@att.net>
2022-04-03 15:36:55 -0400

    Added Scene to test map generation

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,6 +7,8 @@ public class GameManager : MonoBehaviour
     public List<GameObject> PrefabsToSelect;
     public List<GameObject> GeneratedTerrain;
 
+    private float TempTimer;
+
     //public List<int> testList;
 
     void Start()
@@ -22,23 +24,41 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       
+       if(TempTimer > 0) {
+           TempTimer -= Time.deltaTime;
+       } else {
+           TempTimer = 1f;
+           GenerateNextSection(new Vector3(0,0,0));
+       }
     }
 
     public void GenerateNextSection(Vector3 StartTransform)
     {
-        Debug.Log("Generate");
-
         //Currently only selects the straight tile. To change, remove the "- 1"
-        GameObject NextSection = PrefabsToSelect[Random.Range(0, PrefabsToSelect.Count - 1)];
+        GameObject NextSection = PrefabsToSelect[Random.Range(0, PrefabsToSelect.Count)];
         Vector3 EndTransform = GeneratedTerrain[0].transform.Find("Connectors").gameObject.transform.Find("End").transform.position;
+        bool RightTurn;
+        float TurnRotation = 0f;
+
+        if(NextSection == PrefabsToSelect[1]) {
+            Debug.Log("Test");
+            int Rando = Random.Range(0, 2);
+            if(Rando == 0) {
+                RightTurn = true;
+                TurnRotation = 0f;
+            } else {
+                RightTurn = false;
+                TurnRotation = 90f;
+            }
+        }
         
         //This is where the tile is offset
         //More work on this part needed in order to have it offset properly depending on orientation
-        Vector3 PrefabLocation = new Vector3(EndTransform.x, EndTransform.y, (EndTransform.z + 45)); 
+        Vector3 PrefabLocation = new Vector3(EndTransform.x, EndTransform.y, (EndTransform.z)); 
         
-        Destroy(GeneratedTerrain[0]);
-        GeneratedTerrain[0] = Instantiate(NextSection, PrefabLocation, Quaternion.identity); //CurrentTscript.EndTransform.position
+        //Destroy(GeneratedTerrain[0]);
+        GeneratedTerrain[0] = Instantiate(NextSection, PrefabLocation, new Quaternion(0f, TurnRotation, 0f, 1f)); 
+        Debug.Log(GeneratedTerrain[0].transform.rotation);
         
     }
 }

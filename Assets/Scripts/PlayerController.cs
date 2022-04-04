269570f696ef8ea59a1e46ee50cd1ccdf05304c9
Jake <jakegibson0228@gmail.com>
2022-04-04 13:33:28 -0400

    Player changes lanes instead of turning

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -12,6 +13,7 @@ public class PlayerController : MonoBehaviour
     public float speed = 5;
     public float rotationSpeed = 150f;
     private Rigidbody rigidbody;
+    private float timeRemaining;
 
     void Start()
     {
@@ -21,6 +23,12 @@ public class PlayerController : MonoBehaviour
  
     void Update()
     {
+        timeRemaining -= Time.deltaTime;
+        if (timeRemaining <= 0)
+        {
+            startPosition = endPosition = Vector3.zero;
+        }
+        transform.position = new Vector3(Mathf.Lerp(transform.position.x, (endPosition.x > startPosition.x) ? 30 : -30, 0.5f), 0, transform.position.z);
         //Vector3 localTransform = gameObject.transform.localPosition;
         //transform.Translate(transform.forward * speed * Time.deltaTime);
         rigidbody.AddRelativeForce( 0, 0, 1);
@@ -42,16 +50,13 @@ public class PlayerController : MonoBehaviour
             case TouchPhase.Ended:
             {
                 endPosition = touch.position;  //last touch position.
- 
                 //Check if drag distance is greater than 20% of the screen height
                 if (Mathf.Abs(endPosition.x - startPosition.x) > dragDistance || Mathf.Abs(endPosition.y - startPosition.y) > dragDistance)
                 {//It's a drag
                     //check if the drag is vertical or horizontal
                     if (Mathf.Abs(endPosition.x - startPosition.x) > Mathf.Abs(endPosition.y - startPosition.y))
                     {   //If the horizontal movement is greater than the vertical movement...
-                        
-                        transform.RotateAround(transform.position, transform.up, (endPosition.x > startPosition.x) ? 90 : -90);
-                        rigidbody.velocity = Vector3.zero;
+                        timeRemaining = 0.5f;
                     }
                     else
                     {
@@ -67,5 +72,6 @@ public class PlayerController : MonoBehaviour
                 break;
             }
         }
+        
     }
 }

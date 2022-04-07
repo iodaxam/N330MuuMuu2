da27118cd62ed4cae673a7779c065a9039feb2ce
sBrunes <bruness@att.net>
2022-04-06 20:30:30 -0400

    Created Prefab for boundary

---
+++ Assets/Scripts/BoundaryScript.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoundaryScript : MonoBehaviour
+{
+    //public GameObject CubeObject;
+    //public Mesh NewMeshToSet;
+
+    void Start()
+    {
+        //MeshFilter ObjectsFilter = CubeObject.GetComponent<MeshFilter>();
+        //ObjectsFilter.mesh = NewMeshToSet;
+    }
+}

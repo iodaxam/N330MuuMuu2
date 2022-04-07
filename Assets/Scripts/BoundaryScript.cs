955402664ec3bcd153e3ef00c4007eace1860bb1
sBrunes <bruness@att.net>
2022-04-06 22:22:07 -0400

    Land Update

--- Assets/Scripts/BoundaryScript.cs
+++ Assets/Scripts/BoundaryScript.cs
@@ -4,16 +4,54 @@ using UnityEngine;
 
 public class BoundaryScript : MonoBehaviour
 {
-    public List<GameObject> RockObjects;
+    public List<GameObject> SpawnedRockObjects;
+    
     public Mesh LandMesh;
-
     public bool isLand;
+    public GameObject LandObject;
+
+    public List<GameObject> RockPrefabs;
+    public List<Transform> RockLocations;
+
+    public bool isLeft;
 
     void Start()
     {
-        //MeshFilter ObjectsFilter = CubeObject.GetComponent<MeshFilter>();
-        //ObjectsFilter.mesh = NewMeshToSet;
+        if(!isLand) {
+            
+            foreach(Transform SpawnLocation in RockLocations)
+            {
+                GameObject SpawnedRock = Instantiate(RockPrefabs[Random.Range(0, RockPrefabs.Count)], SpawnLocation.position, Quaternion.identity);
+                SpawnedRockObjects.Add(SpawnedRock);
 
-        Debug.Log(isLand);
+                SpawnedRock.transform.localScale = new Vector3(5f, 5f, 5f);
+            }
+
+        } else {
+
+            MeshFilter LandMeshFilter = LandObject.GetComponent<MeshFilter>();
+            LandMeshFilter.mesh = LandMesh;
+
+            if(!isLeft)
+            {
+                Vector3 OriginalPosition = LandObject.transform.position;
+                LandObject.transform.position = new Vector3(200, OriginalPosition.y, OriginalPosition.z);
+
+                LandObject.transform.rotation = Quaternion.Euler(0, 90, 0);
+            }
+        }
+    }
+
+    void OnDestroy()
+    {
+        if(!isLand)
+        {
+            foreach(GameObject Rock in SpawnedRockObjects)
+            {
+                Destroy(Rock.gameObject);
+            }
+        } else {
+            Destroy(LandObject);
+        }
     }
 }

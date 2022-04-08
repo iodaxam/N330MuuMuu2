dd8e6613b14a87314b6fe13bfd9582f79441c5d6
sBrunes <bruness@att.net>
2022-04-08 14:36:54 -0400

    Started on fog

---
+++ Assets/Scripts/FogParentScript.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FogParentScript : MonoBehaviour
+{
+    public Transform PlayerTransform;
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.position = new Vector3(0f, 0f, PlayerTransform.position.z);
+    }
+}

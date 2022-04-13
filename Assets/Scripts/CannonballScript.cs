86154118b1bf32037e9db575e9f72729ab26b513
sBrunes <bruness@att.net>
2022-04-13 02:51:15 -0400

    Added Water Splash particle effect

--- Assets/Scripts/CannonballScript.cs
+++ Assets/Scripts/CannonballScript.cs
@@ -4,18 +4,23 @@ using UnityEngine;
 
 public class CannonballScript : MonoBehaviour
 {
-    public GameObject ParticleEffect;
+    public GameObject Rock_ParticleEffect;
+    public GameObject Water_ParticleEffect;
 
     void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Terrain")
         {
+            GameObject PE = Instantiate(Water_ParticleEffect, gameObject.transform.position, Quaternion.identity);
+
+            Destroy(PE, 2);
+
             Destroy(gameObject);
         } else if(other.tag == "Obstacle")
         {
             Destroy(other.gameObject);
 
-            GameObject PE = Instantiate(ParticleEffect, other.transform.position, Quaternion.identity);
+            GameObject PE = Instantiate(Rock_ParticleEffect, other.transform.position, Quaternion.identity);
 
             Destroy(PE, 2);
 

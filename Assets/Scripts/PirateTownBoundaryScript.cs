0f07107f7d90c9b2c5a3038e75f9df9e703edaaf
sBrunes <bruness@att.net>
2022-04-22 03:09:52 -0400

    Pirate Town Update

--- Assets/Scripts/PirateTownBoundaryScript.cs
+++ Assets/Scripts/PirateTownBoundaryScript.cs
@@ -7,9 +7,21 @@ public class PirateTownBoundaryScript : MonoBehaviour
     public bool isLeft;
     public GameObject LandParent;
 
+    public List<Mesh> Platforms;
+    public List<GameObject> PlatformObjects;
+
     void Start()
     {
-       if(!isLeft)
+        foreach(GameObject Plank in PlatformObjects)
+        {
+            int RandNumber = Random.Range(0, Platforms.Count);
+
+            MeshFilter Filter = Plank.GetComponent<MeshFilter>();
+
+            Filter.mesh = Platforms[RandNumber];
+        }
+
+        if(!isLeft)
         {   
             LandParent.transform.localRotation = Quaternion.Euler(0, 180, 0);
             LandParent.transform.localPosition = new Vector3(0, 0, 150);

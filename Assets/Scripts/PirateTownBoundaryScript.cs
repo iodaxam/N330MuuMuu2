c78aebe852aa70175ac226fa4e23e444dfd25447
sBrunes <bruness@att.net>
2022-04-23 04:18:04 -0400

    Piratetown update

--- Assets/Scripts/PirateTownBoundaryScript.cs
+++ Assets/Scripts/PirateTownBoundaryScript.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class PirateTownBoundaryScript : MonoBehaviour
 {
+    private List<GameObject> AddedObjects;
+
     public bool isLeft;
     public GameObject LandParent;
 
     public List<Mesh> Platforms;
     public List<GameObject> PlatformObjects;
+    public List<GameObject> BuildingObjects;
+    public List<Transform> BuildingLocations;
 
     void Start()
     {
@@ -29,6 +33,20 @@ public class PirateTownBoundaryScript : MonoBehaviour
         {   
             LandParent.transform.localRotation = Quaternion.Euler(0, 180, 0);
             LandParent.transform.localPosition = new Vector3(0, 0, 150);
+        }
+
+        StartCoroutine(LateStart(.1f));
+    }
+
+    IEnumerator LateStart(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+
+        foreach(Transform BuildingLocation in BuildingLocations)
+        {
+            int RandNumber = Random.Range(0, BuildingObjects.Count);
+
+            AddedObjects.Add(Instantiate(BuildingObjects[RandNumber], BuildingLocation.position, Quaternion.identity));
         }
     }
 }  

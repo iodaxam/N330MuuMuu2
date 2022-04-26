a5af9d043851423ac6590ed1d97d98b36ff585ff
sBrunes <bruness@att.net>
2022-04-26 00:59:40 -0400

    Update

--- Assets/Scripts/VolcanoBoundaryScript.cs
+++ Assets/Scripts/VolcanoBoundaryScript.cs
@@ -21,6 +21,11 @@ public class VolcanoBoundaryScript : MonoBehaviour
 
     void Start()
     {
+        if(!isLeft)
+       {
+           LandParent.transform.rotation = Quaternion.Euler(0, 180, 0);
+       }
+
         foreach(GameObject Location in FrontLocations)
         {
             if(Random.Range(0,2) == 0)
@@ -34,6 +39,7 @@ public class VolcanoBoundaryScript : MonoBehaviour
                 GameObject ColliderChild = Location.transform.Find("MeshColliderThing").gameObject;
 
                 ColliderChild.GetComponent<MeshCollider>().sharedMesh = SandMeshs[RandNumber];
+
             } else if(Random.Range(0,2) == 0) {
                 int RandNumber = Random.Range(0, RockMeshs.Count);
 
@@ -43,12 +49,22 @@ public class VolcanoBoundaryScript : MonoBehaviour
 
                 Location.transform.localScale = new Vector3(3, 3, 3);
             }
+
+            Location.transform.localScale += new Vector3(0, 0, Random.Range(-1, 2));
         }
 
-       if(!isLeft)
-       {
-           LandParent.transform.rotation = Quaternion.Euler(0, 180, 0);
-       }
+        foreach(Transform Location in BackLocations)
+        {
+            int RandNumber = Random.Range(0, BackPrefabs.Count);
+
+            GameObject SpawnedObject = Instantiate(BackPrefabs[RandNumber], Location.position, Quaternion.Euler(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360)));
+
+            AddedObjects.Add(SpawnedObject);
+
+            SpawnedObject.transform.localScale = new Vector3(4, 4, 4);
+
+            Location.transform.localScale += new Vector3(0, 0, Random.Range(-1, 2));
+        }
     }
 
     void OnDestroy()

c036599366f8ee4b38fafe2c1f8221a0692c923d
sBrunes <bruness@att.net>
2022-04-26 14:27:54 -0400

    Volcano explosion

---
+++ Assets/Scripts/VolcanoScript.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolcanoScript : MonoBehaviour
+{
+    public Transform ExpLocation;
+    public GameObject ExplosionPS;
+
+    void OnTriggerEnter(Collider other)
+	{
+        Instantiate(ExplosionPS, ExpLocation.position, Quaternion.identity);
+    }
+}
